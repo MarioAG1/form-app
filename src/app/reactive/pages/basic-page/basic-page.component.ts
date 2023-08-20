5db24ebba53d2bef81410b99a5698fdfe8b62384
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-20 19:51:35 +0200

    Metodos de ayuda para los formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -33,14 +33,36 @@ export class BasicPageComponent implements OnInit {
     // this.myForm.reset(byDefault)
   }
 
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) {
+      return null
+    }
+    const errors = this.myForm.controls[field].errors || {}
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case "required":
+          return "Este campo es requerido"
+        case "minlength":
+          return `Minimo ${errors["minlength"].requiredLength} caracteres`
+      }
+    }
+    return null
+  }
+
   onSave(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched()
       return
     }
-
-
     console.log(this.myForm.value)
   }
+
 
 }

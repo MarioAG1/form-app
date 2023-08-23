63fe77190c5a328220a6c695ccf778104b104b0c
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-23 16:48:58 +0200

    Terminada la tarea de la persona en el formulario

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
@@ -6,6 +7,49 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ["M", [Validators.required]],
+    wantNotifications: [true, [Validators.required]],
+    termsAndConditions: [true, [Validators.requiredTrue]]
+  })
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  public person = {
+    gender: "F",
+    wantNotifications: false
+  }
+
+  constructor(private fb: FormBuilder) {
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  OnInit() {
+    this.myForm.reset(this.person)
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched()
+      return
+    }
+
+    //Para que no se añada el terms y condiciones a la persona
+    const { termsAndConditions, ...newPerson } = this.myForm.value
+    this.person = newPerson
+    console.log(this.myForm.value)
+    console.log(this.person)
+
+
+
+  }
 
 }

1a024e1b3fde636ad6b159be7b1df642db95158f
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-23 22:35:27 +0200

    Formulario de registro, etapa inicial

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ["", [Validators.required]],
+    email: ["", [Validators.required]],
+    username: ["", [Validators.required]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    password2: ["", [Validators.required]]
+  })
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  isValidField(field: string) {
+
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched
+  }
 
 }

bd580a552c8797803efd53d57a584c8cee031e23
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-24 22:42:50 +0200

    Validaciones de email y Patrones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider } from 'src/app/shared/validators/validators';
+import * as customValidators from "../../../shared/validators/validators"
 
 @Component({
   selector: 'app-register-page',
@@ -11,9 +12,9 @@ import { cantBeStrider } from 'src/app/shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ["", [Validators.required]],
-    email: ["", [Validators.required]],
-    username: ["", [Validators.required, cantBeStrider]],
+    name: ["", [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ["", [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ["", [Validators.required, customValidators.cantBeStrider]],
     password: ["", [Validators.required, Validators.minLength(6)]],
     password2: ["", [Validators.required]]
   })
--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,6 +1,9 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, ValidationErrors } from "@angular/forms";
 
-export const cantBeStrider = (control: FormControl) => {
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+export const cantBeStrider = (control: FormControl): ValidationErrors | null => {
 
   const value: string = control.value.trim().toLowerCase()
 

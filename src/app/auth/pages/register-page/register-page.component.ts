6839af18bfb6a7a9a3d36a0f601a5c4b6adef85e
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-25 14:28:38 +0200

    Validators Service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from 'src/app/shared/validators/validators';
-import * as customValidators from "../../../shared/validators/validators"
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 
 @Component({
   selector: 'app-register-page',
@@ -12,19 +11,19 @@ import * as customValidators from "../../../shared/validators/validators"
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ["", [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email: ["", [Validators.required, Validators.pattern(customValidators.emailPattern)]],
-    username: ["", [Validators.required, customValidators.cantBeStrider]],
+    name: ["", [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ["", [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    username: ["", [Validators.required, this.validatorsService.cantBeStrider]],
     password: ["", [Validators.required, Validators.minLength(6)]],
     password2: ["", [Validators.required]]
   })
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private validatorsService: ValidatorsService) {
 
   }
 
   isValidField(field: string) {
-
+    return this.validatorsService.isValidField(this.myForm, field)
   }
 
   onSubmit() {
--- src/app/shared/services/validators.service.ts
+++ src/app/shared/services/validators.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidatorsService {
 
-  constructor() { }
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  public cantBeStrider = (control: FormControl): ValidationErrors | null => {
+
+    const value: string = control.value.trim().toLowerCase()
+
+    if (value === "strider") {
+      return {
+        noStrider: true
+      }
+    }
+    return null
+  }
+
+  public isValidField(form: FormGroup, field: string) {
+    return form.controls[field].errors && form.controls[field].touched
+  }
+
+
 }
